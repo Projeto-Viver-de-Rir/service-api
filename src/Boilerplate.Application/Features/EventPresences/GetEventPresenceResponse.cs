fed2820830056c1965c3202bc590547b434fd4d3
Lucas Felipe Kaspary <lucasfelipekaspary@gmail.com>
2024-05-19 12:27:23 +0100

    Fake mock name until correct mapping

--- src/Boilerplate.Application/Features/EventPresences/GetEventPresenceResponse.cs
+++ src/Boilerplate.Application/Features/EventPresences/GetEventPresenceResponse.cs
@@ -9,6 +9,8 @@ public record GetEventPresenceResponse
     public EventPresenceId Id { get; set; }
     public EventId EventId { get; set; }
     public VolunteerId VolunteerId { get; set; }
+    public string Name { get; set; } = "Volunteer Cool Name";
+    public string Photo { get; set; }
     public DateTime RegistrationAt { get; set; }
     public bool Attended { get; set; }
 }
