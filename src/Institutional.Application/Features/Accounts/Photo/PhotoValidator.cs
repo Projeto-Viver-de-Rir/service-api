1054cbddfca202eee264a532e09f49c74f88b89c
Lucas Felipe Kaspary <lucasfelipekaspary@gmail.com>
2024-08-26 19:51:11 +0100

    Validation for supported image files on profile photo.

---
+++ src/Institutional.Application/Features/Accounts/Photo/PhotoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Institutional.Application.Features.Accounts.Photo;
+
+public class PhotoValidator : AbstractValidator<PhotoRequest>
+{
+    public PhotoValidator()
+    {
+        RuleLevelCascadeMode = ClassLevelCascadeMode;
+
+        RuleFor(x => x.FileExtension)
+            .NotEmpty()
+            .Must(x => x.Equals(".jpg") || x.Equals(".jpeg") || x.Equals(".png"));
+    }
+}
