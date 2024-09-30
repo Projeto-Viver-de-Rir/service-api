363bbcdfee84572fa62afe9d887d62507222b80e
Lucas Felipe Kaspary <lucasfelipekaspary@gmail.com>
2024-09-30 23:48:57 +0100

    Photo for team members

--- src/Institutional.Application/Features/Teams/GetAllTeams/GetAllTeamsHandler.cs
+++ src/Institutional.Application/Features/Teams/GetAllTeams/GetAllTeamsHandler.cs
@@ -13,10 +13,12 @@ namespace Institutional.Application.Features.Teams.GetAllTeams;
 public class GetAllTeamsHandler : IRequestHandler<GetAllTeamsRequest, PaginatedList<GetTeamResponse>>
 {
     private readonly IContext _context;
+    private readonly IStorageService _storageService;
     
-    public GetAllTeamsHandler(IContext context)
+    public GetAllTeamsHandler(IContext context, IStorageService storageService)
     {
         _context = context;
+        _storageService = storageService;
     }
     public async Task<PaginatedList<GetTeamResponse>> Handle(GetAllTeamsRequest request, CancellationToken cancellationToken)
     {
@@ -24,8 +26,16 @@ public class GetAllTeamsHandler : IRequestHandler<GetAllTeamsRequest, PaginatedL
             .Include(p => p.Members)
             .WhereIf(!string.IsNullOrEmpty(request.Name), x => EF.Functions.Like(x.Name, $"%{request.Name}%"))
             .WhereIf(request.TeamType != null, x => x.Type == request.TeamType);
-        return await teams.ProjectToType<GetTeamResponse>()
+        
+        var paginatedListAsync = await teams.ProjectToType<GetTeamResponse>()
             .OrderBy(x => x.Name)
             .ToPaginatedListAsync(request.CurrentPage, request.PageSize);
+
+        foreach (var item in paginatedListAsync.Result)
+            foreach (var member in item.Members)
+                if (!string.IsNullOrWhiteSpace(member.Volunteer.Photo))
+                    member.Volunteer.Photo = await _storageService.GetFilePathAsync(member.Volunteer.Photo);
+
+        return paginatedListAsync;
     }
 }
